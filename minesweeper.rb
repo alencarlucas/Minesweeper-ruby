08dc097be871967b7a4d3e321ee1048f5bef4e35
alencarlucas <alencarlucaas@gmail.com>
2017-09-04 09:32:01 -0300

    class board added

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,8 +1,31 @@
-class Minesweeper
-  attr_accessor :width, :height, :num_mines
-  def initialize width, height = width, num_mines = width
-      @width = width
-      @height = height
+class Board
+    attr_accessor :width, :height
+
+    def initialize(x,y)
+        @width = x
+        @height = y
+        @hashBoard = Hash.new(0)
+    end
+
+    def coordinateToIndex(xi,yi)
+        xi*(@width*@height)+yi
+    end
+
+    def setRandomMines(maxMines)
+        [*0..@width].sample(maxMines).zip([*0..@height].sample(maxMines)).each do |x,y|
+            @hashBoard[coordinateToIndex(x.to_i,y.to_i)] = 1
+        end
+    end
+
+end
+
+class Minesweeper < Board
+  attr_accessor :num_mines, :board
+
+  def initialize width = 8, height = 8, num_mines = 10
       @num_mines = num_mines
+      @board = Board.new(width,height)
+      @board.setRandomMines(num_mines)
   end
+
 end
--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -8,9 +8,15 @@ describe Minesweeper do
     context "when all parameters are specified" do
       it "width == 10, height == 5, num_mines == 15" do
         minesweeper = Minesweeper.new(10, 5, 15)
-        expect(minesweeper.width).to eq(width)
-        expect(minesweeper.height).to eq(height)
+        expect(minesweeper.board.width).to eq(width)
+        expect(minesweeper.board.height).to eq(height)
         expect(minesweeper.num_mines).to eq(num_mines)
+      end
+      it "no params (default board 8x8 10 mines)" do
+        minesweeper = Minesweeper.new()
+        expect(minesweeper.board.width).to eq(8)
+        expect(minesweeper.board.height).to eq(8)
+        expect(minesweeper.num_mines).to eq(10)
       end
     end
   end
