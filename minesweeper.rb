dcd04478c801442a56882f1041467e28d131e574
alencarlucas <alencarlucaas@gmail.com>
2017-09-04 12:02:48 -0300

    exceptions added

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,19 +1,31 @@
-class Board
-    attr_accessor :width, :height
+class ArgumentError < StandardError
 
+end
+
+#class to represent the minesweeper board
+class Board
+    attr_accessor :width, :height, :hashBoard
+
+    #initialize the board with size parameters
+    #set all positions equals 0 (empty cell)
     def initialize(x,y)
-        @width = x
-        @height = y
+        @width, @height = x, y
         @hashBoard = Hash.new(0)
     end
 
-    def coordinateToIndex(xi,yi)
-        xi*(@width*@height)+yi
+    def [](i)
+        @hashBoard[i.to_sym]
     end
 
+    #function used to refer a pair as a key
+    def pairToKey(xi,yi)
+        (xi*(@width*@height)+yi)
+    end
+
+    #set random cells equals 1 (bomb position)
     def setRandomMines(maxMines)
         [*0..@width].sample(maxMines).zip([*0..@height].sample(maxMines)).each do |x,y|
-            @hashBoard[coordinateToIndex(x.to_i,y.to_i)] = 1
+            @hashBoard[pairToKey(x.to_i,y.to_i)] = 1
         end
     end
 
@@ -23,9 +35,10 @@ class Minesweeper < Board
   attr_accessor :num_mines, :board
 
   def initialize width = 8, height = 8, num_mines = 10
-      @num_mines = num_mines
-      @board = Board.new(width,height)
-      @board.setRandomMines(num_mines)
+    raise ArgumentError.new "all params need be numbers" if [width,height,num_mines].all? {|i| not(i.is_a? Numeric) }
+    raise ArgumentError.new "all params need be greater than zero" if ([width,height,num_mines].min.to_i < 0)
+    @num_mines = num_mines
+    @board = Board.new(width,height)
+    @board.setRandomMines(num_mines)
   end
-
 end
--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -1,22 +1,79 @@
 require_relative '../minesweeper'
 
+describe Board do
+  context "#new" do
+    let(:width) {10}
+    let(:height) {5}
+    let(:arr) {[0]*3}
+    context "verify initialization" do
+      it "width == 10, height == 5 and board[key] == 0" do
+        board = Board.new(width, height)
+        bHash = board.hashBoard
+        expect(board.width).to eq(width)
+        expect(board.height).to eq(height)
+        expect([bHash[0],bHash[1000000],bHash[42]]).to include(*arr)
+      end
+    end
+  end
+  context "#pairToKey" do
+    context "test cases" do
+      board = Board.new(100000, 500)
+      it "x == 0, y == 0, return 0" do
+        expect(board.pairToKey(0,0)).to eq(0)
+      end
+      it "x == 10, y == 5, return 0" do
+      expect(board.pairToKey(10,5)).to eq(500000005)
+      end
+      it "x == 3, y == 2, return 0" do
+      expect(board.pairToKey(3,2)).to eq(150000002)
+      end
+    end
+  end
+end
+
 describe Minesweeper do
-  let (:width) {10}
-  let(:height) {  5}
+  let(:width) {10}
+  let(:height) {5}
   let(:num_mines) {15}
   context "#new" do
     context "when all parameters are specified" do
       it "width == 10, height == 5, num_mines == 15" do
-        minesweeper = Minesweeper.new(10, 5, 15)
+        minesweeper = Minesweeper.new(width, height, num_mines)
         expect(minesweeper.board.width).to eq(width)
         expect(minesweeper.board.height).to eq(height)
         expect(minesweeper.num_mines).to eq(num_mines)
       end
-      it "no params (default board 8x8 10 mines)" do
+    end
+    context "no params" do
+      it "default board 8x8 10 mines" do
         minesweeper = Minesweeper.new()
         expect(minesweeper.board.width).to eq(8)
         expect(minesweeper.board.height).to eq(8)
         expect(minesweeper.num_mines).to eq(10)
+      end
+    end
+    context "Test invalid params" do
+      context "Pass params less than zero" do
+        it "width == -1 raise ArgumentError" do
+          expect{Minesweeper.new(-1,0,3)}.to raise_error(ArgumentError)
+        end
+        it "num_mines == -33 raise ArgumentError" do
+          expect{Minesweeper.new(1,0,-33)}.to raise_error(ArgumentError)
+        end
+        it "all params less than zero raise ArgumentError" do
+          expect{Minesweeper.new(-1,-11,-3)}.to raise_error(ArgumentError)
+        end
+      end
+      context "Pass params not numbers" do
+        it "width == 'q' raise ArgumentError" do
+          expect{Minesweeper.new('q',0,3)}.to raise_error(ArgumentError)
+        end
+        it "num_mines == 'vinte' raise ArgumentError" do
+          expect{Minesweeper.new(1,0,'vinte')}.to raise_error(ArgumentError)
+        end
+        it "all params not numbers raise ArgumentError" do
+          expect{Minesweeper.new("1",'11','3')}.to raise_error(ArgumentError)
+        end
       end
     end
   end
