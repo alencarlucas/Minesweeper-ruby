fbd149041583852e6c98d8eec358e9dfda0cadc3
Lucas Alencar <alencarlucaas@gmail.com>
2017-09-04 22:20:28 -0300

    play method added

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,58 +1,94 @@
-
-
 #class to represent the minesweeper board
 class Board
-    attr_accessor :width, :height, :hashBoard
+  attr_accessor :width, :height, :clicked_a_mine, :hashBoard
 
-    #initialize the board with size parameters
-    #set all positions equals 0 (empty cell)
-    def initialize(x,y)
-        @width, @height = x, y
-        @hashBoard = Hash.new('.')
-        @hashmine = Hash.new()
-        @clicked_a_mine = false
-    end
+  #initialize the board with size parameters
+  #set all positions equals 0 (empty cell)
+  def initialize(x,y)
+    @width, @height = x, y
+    @hashBoard = Hash.new('.')
+    @hashmine = Hash.new()
+    @clicked_a_mine = false
+  end
 
-    def [](i)
-      @hashBoard[i]
-    end
+  def [](i)
+    @hashBoard[i]
+  end
 
-    def [](x,y)
-      @hashBoard[pairToKey(x,y)]
-    end
+  def [](x,y)
+    @hashBoard[pairToKey(x,y)]
+  end
 
-    def verify_mine index, value
-      clicked_a_mine = @hashmine[index] != '#' || value != ''
-    end
+  def verify_mine index, value
+    @hashmine[index] != '#' || value != ' '
+  end
 
-    def []=(x,y,value)
-      index = pairToKey(x,y)
-      if(verify_mine(index, value))
-        @hashBoard[pairToKey(x,y)] = value
-      else
-        false
+  def unmark
+    [*1..@width*@height].each { |e| @hashBoard[e] = (@hashBoard[e] == 'xF' ? "F" : @hashBoard[e]) }
+  end
+
+  def verify_neighbor x, y
+    flagShow = true
+    arr = Array.new()
+    [-1,1,0].product([1,-1,0]).each do |pair|
+      if(pair[0] != 0 || pair[1] != 0 )
+        neighbor = [pair[0]+x, pair[1]+y]
+        index = pairToKey(neighbor[0],neighbor[1])
+        if([*1..@width*@height].include?(index) && @hashmine[index] != '#')
+          #push [[x,y],index]
+          arr.push([neighbor,index])
+        else
+          flagShow = false
+        end
       end
     end
-
-    #function used to refer a pair as a key
-    def pairToKey(xi,yi)
-      (xi*(@height)+yi)
+    if(flagShow)
+      arr.each do |neighbor|
+        if(@hashBoard[neighbor[1]] == '.' || @hashBoard[neighbor[1]] == 'F')
+          @hashBoard[neighbor[1]] = (@hashBoard[neighbor[1]] == 'F' ? "xF" : ' ')
+          verify_neighbor neighbor[0][0], neighbor[0][1]
+        end
+      end
     end
+  end
 
-    #set random cells equals 1 (mine position)
-    def setRandomMines(maxMines)
-      [*0..@width-1].product([*0..@height-1]).sample(maxMines).each do |pair|
+  def []=(x,y,value)
+    index = pairToKey(x,y)
+    if(verify_mine(index, value))
+      @hashBoard[index] = value
+      if(value == ' ')
+        verify_neighbor x, y
+        unmark
+      end
+    else
+      @hashBoard[index] = 'X'
+      @clicked_a_mine = true
+      false
+    end
+  end
+
+  #function used to refer a pair as a key
+  def pairToKey(xi,yi)
+    (xi*(@height)+yi)
+  end
+
+  #set random cells equals 1 (mine position)
+  def setRandomMines(maxMines)
+    [*0..@width-1].product([*0..@height-1]).sample(maxMines).each do |pair|
+        if(pair[0] != 0 && pair[1] != 0)
           @hashmine[pairToKey(pair[0].to_i,pair[1].to_i)] = '#'
-      end
+          puts pairToKey(pair[0].to_i,pair[1].to_i)
+        end
     end
+  end
 
-    def getCellStatus index
-        @hashmine[index] == '#' ? '#' : @hashBoard[index]
-    end
+  def getCellStatus index
+      @hashmine[index] == '#' ? '#' : @hashBoard[index]
+  end
 
 end
 
-class Minesweeper < Board
+class Minesweeper
   attr_accessor :num_mines, :size, :board, :valid_plays
 
   def initialize width = 8, height = 8, num_mines = 10
@@ -68,7 +104,15 @@ class Minesweeper < Board
   end
 
   def still_playing?
-    clicked_a_mine || victory?
+    !@board.clicked_a_mine || victory?
+  end
+
+  def play x,y
+    if @board[x,y] == '.'
+      @board[x,y] = ' '
+    else
+      false
+    end
   end
 
   def SimplePrinter
@@ -93,4 +137,37 @@ class Minesweeper < Board
   end
 end
 
-Minesweeper.new().SimplePrinter
+game = Minesweeper.new()
+
+game.SimplePrinter
+game.flag 1,3
+if game.play 1,2
+  puts ''
+  game.SimplePrinter
+end
+# if game.play 5,6
+#   puts ''
+#   game.SimplePrinter
+# end
+# if game.play 4,8
+#   puts ''
+#   game.SimplePrinter
+# end
+# if game.play 3,3
+#   puts ''
+#   game.SimplePrinter
+# end
+# if game.play 1,4
+#   puts ''
+#   game.SimplePrinter
+# end
+
+if(!game.still_playing?)
+  if game.victory?
+    puts 'venceu'
+  else
+    puts 'perdeu'
+  end
+else
+  puts 'continua'
+end
